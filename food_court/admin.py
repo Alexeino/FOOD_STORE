55b02676d81dbc393df5ecd777f0b07eb6c264f6
Alexeino <ms8750122327@gmail.com>
2021-07-16 07:59:50 +0530

    Added Relational Models, 1-1, 1-Many & Many-Many. App complete at backend for now...

--- food_court/admin.py
+++ food_court/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Address, City, Food, Sheff
 # Register your models here.
+
+
+class FoodAdmin(admin.ModelAdmin):
+    # readonly_fields=("slug",)
+    prepopulated_fields = {"slug": ("dish",)}
+    list_filter = ("rating","is_popular","sheff")
+    list_display = ("id","dish","sheff")
+    sortable_by = ("id")
+
+class SheffAdmin(admin.ModelAdmin):
+    list_display=("first_name","last_name")
+
+class AddressAdmin(admin.ModelAdmin):
+    # list_display=("street","city","postal_code")
+    pass
+
+
+admin.site.register(Food,FoodAdmin)
+admin.site.register(Sheff,SheffAdmin)
+admin.site.register(Address,AddressAdmin)
+admin.site.register(City)
--- food_court/models.py
+++ food_court/models.py
@@ -1,25 +1,59 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import BooleanField, SlugField
 from django.utils.text import slugify
 from django.urls import reverse
 # Create your models here.
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=2)
+
+    class Meta:
+        verbose_name_plural = "City Enteries"
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=8)
+    city = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Adress Enteries"
+
+    def __str__(self):
+        return f"{self.street} {self.city}, {self.postal_code}"
+
+class Sheff(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name  = models.CharField(max_length=100,blank=True)
+    address = models.OneToOneField(Address,on_delete=CASCADE, null=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.address}"
+
 class Food(models.Model):
     dish = models.CharField(max_length=100)
     rating = models.FloatField(validators=[MinValueValidator(0),MaxValueValidator(5)])
     is_popular = BooleanField(default=False)
-    slug = SlugField(default="",null=False,db_index=True)
+    sheff = models.ForeignKey(Sheff, on_delete=models.CASCADE,related_name="foods")
+    slug = SlugField(default="",blank=True,null=False,db_index=True)
+    origin_city = models.ManyToManyField(City)
 
 
     def __str__(self):
         return f"[{self.id}.]       {self.dish}, Rating:{self.rating}"
     
-    def save(self, *args,**kwargs):
-        self.slug = slugify(self.dish)
+    # def save(self, *args,**kwargs):
+    #     self.slug = slugify(self.dish)
 
-        super().save(*args,**kwargs)
+    #     super().save(*args,**kwargs)
 
     def get_absolute_url(self):
         return reverse("food_detail",args=[self.slug])
 
-    
+
